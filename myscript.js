fe9f99ba8d18d91a2ca6b36afe29a0936de0256e
harshitaksc <harshitaksc@gmail.com>
2025-01-29 20:35:44 +0530

    Add interactive painting function to page

--- myscript.js
+++ myscript.js
@@ -4,5 +4,19 @@ for (let i=1; i <= 256; i++){
     const div = document.createElement('div');
     container.appendChild(div);
     div.textContent = i;
+    
     }
+
+const paint = container.childNodes;
+// console.log(paint);
+
+
+paint.forEach(el => el.addEventListener('mouseover', event => {
+    el.style.backgroundColor = "green";
+  }));
+  
+
+    
+
+    
    
