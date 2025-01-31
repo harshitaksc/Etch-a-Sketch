4d34a5b8804d97f4dfa0654e0d9958ad38278abf
harshitaksc <harshitaksc@gmail.com>
2025-01-31 15:15:37 +0530

    Add user input of grid amount

--- myscript.js
+++ myscript.js
@@ -1,18 +1,30 @@
 const container = document.querySelector("#container");
 
-for (let i=1; i <= 256; i++){
+// Take input of the grid number user desires
+let grid = parseInt(prompt("How many grids do you want?"), 10);
+
+if (isNaN(grid) || grid < 1 || grid > 100) {
+    alert("Invalid input! Using default grid size: 16.");
+    grid = 16; 
+}
+
+console.log(grid);
+for (let i=1; i <= grid*grid; i++){
     const div = document.createElement('div');
     container.appendChild(div);
-    div.textContent = i;
-    
+    div.style.width = `${100 / grid}%`;
+        
     }
+   
 
 const paint = container.childNodes;
 // console.log(paint);
 
 
+
 paint.forEach(el => el.addEventListener('mouseover', event => {
-    el.style.backgroundColor = "green";
+    el.style.backgroundColor = "chocolate";
+    
   }));
   
 
